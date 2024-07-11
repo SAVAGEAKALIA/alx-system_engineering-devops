62e970b9c72c1eae1c0d0c7290333d7dcbec795b
Saviour <akaliasaviour@gmail.com>
2024-07-11 10:39:42 +0100

    ok

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,28 @@
+#!/usr/bin/env bash
 # 2-puppet_custom_http_response_header.pp
 
 # Ensure Nginx is installed
-package { 'nginx':
-  ensure => installed,
-}
+#package { 'nginx':
+#  ensure => installed,
+#}
 
 # Ensure the add_header directive is added to the Nginx configuration
 exec { 'add_custom_header':
-  command => 'sudo apt-get update -y', '/bin/sed -i "/location \//a \\            add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default',
+  command => 'sudo apt-get update -y; 
+  sudo apt-get install nginx -y;
+  sudo /bin/sed -i "/location \//a \\            add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default
+  sudo service nginx restart',
+  provider => shell,
   #command => '/bin/sed -i "/server_name _;/a \\        add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-  unless  => '/bin/grep -q "add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+  #unless  => '/bin/grep -q "add_header X-Served-By $(hostname);" /etc/nginx/sites-available/default',
+  #  require => Package['nginx'],
+  # notify  => Service['nginx'],
 }
 
 # Ensure the Nginx service is running and enabled
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => Exec['add_custom_header'],
-}
+#service { 'nginx':
+#  ensure    => running,
+#  enable    => true,
+#  subscribe => Exec['add_custom_header'],
+#}
 
