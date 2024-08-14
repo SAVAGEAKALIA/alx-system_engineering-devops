27fbbe439cd5cd3e2a571bc751bb86396ebc6aaa
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-08-14 03:04:28 +0100

    api advance 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+"""Api implementation using Reddit"""
+import requests
+# import json
+
+
+def number_of_subscribers(subreddit):
+    """Return total number of subscribers"""
+    # print(subreddit)
+    url = "https://www.reddit.com/r/"
+    req = requests.get(f'{url}{subreddit}/about.json', allow_redirects=False)
+    content_type = req.headers.get('Content-Type', '')
+    # print(content_type)
+    # print(req.json())
+    # print(json.dumps(req.json(), indent=4))
+    if 'application/json' in content_type:
+        try:
+            # data = dir(req)
+            # print(data)
+            if req.status_code == 200:
+                return int(req.json()['data']['subscribers'])
+            else:
+                # print(f"Error: status code {req.status_code}")
+                return 0
+        except Exception as e:
+            print(f"Error: {e}")
+    else:
+        # print("Error: not in json format")
+        return 0
+
+
+# if __name__ == '__main__':
+#     number_of_subscribers('programming')
