70677a9e4cec4fefd9ad1c354fa47d5ae1d8cf3d
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-08-15 03:35:06 +0100

    webstack debugging

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,14 @@
+# Ensure the WordPress configuration is fixed
+exec { 'update-wp-settings':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
+  unless  => 'grep -q php /var/www/html/wp-settings.php',
+}
+
+# Restart Apache to apply changes
+exec { 'restart-apache':
+  command => 'sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin'],
+  require => Exec['update-wp-settings'],
+}
