3402a4a4fc003738d2de46db89bb2d733b4dcea4
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-08-15 03:18:36 +0100

    api-advance 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,18 +10,11 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "0-subs-script/0.1"}
     req = requests.get(f'{url}{subreddit}/about.json',
                        headers=headers, allow_redirects=False)
-    content_type = req.headers.get('Content-Type', '')
 
-    if 'application/json' in content_type:
-        try:
-            if req.status_code == 200:
-                data = req.json()
-                count = data['data']['subscribers']
-                # print(type(count))
-                return count
-            return 0
-        except Exception as e:
-            print(f"Error: {e}")
-    else:
-        # print("Error: not in json format")
+    if req.status_code != 200:
         return 0
+
+    data = req.json()
+    count = data['data']['subscribers']
+    # print(type(count))
+    return count
