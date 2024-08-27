956e6478be8db6572b36bf382db406a2fc8858c7
Saviour <akaliasaviour@gmail.com>
2024-08-27 16:30:05 +0100

    webstack 4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -6,9 +6,9 @@ $ulimit_new = 'ULIMIT="-n 4096"'
 exec {'increase_request_limit':
         provider => shell,
         command  => "sudo sed -i 's/${ulimit_old}/${ulimit_new}/' ${nginx_config_file}",
-        before   => Exec['reload'],
-        onlyif   => 'test -f /etc/default/nginx'
+        onlyif   => 'test -f /etc/default/nginx',
         path     => ['/bin', '/usr/bin', '/usr/local/bin'],
+        before   => Exec['reload'],
 }
 
 exec {'reload':
