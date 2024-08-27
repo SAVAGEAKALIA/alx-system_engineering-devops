8dcf89c2d7bed5860883a1fd200004e56561414a
Saviour <akaliasaviour@gmail.com>
2024-08-27 16:36:25 +0100

    webstack 4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # Fix high amount of requests problems
 $nginx_config_file = '/etc/default/nginx'
 $ulimit_old = 'ULIMIT="-n [0-9]*"'
-$ulimit_new = 'ULIMIT="-n 4096"'
+$ulimit_new = 'ULIMIT="-n 65535"'
 
 exec {'increase_request_limit':
         provider => shell,
-        command  => "sudo sed -i 's/${ulimit_old}/${ulimit_new}/' ${nginx_config_file}",
+        command  => "sed -i 's/${ulimit_old}/${ulimit_new}/' ${nginx_config_file}",
         onlyif   => 'test -f /etc/default/nginx',
         path     => ['/bin', '/usr/bin', '/usr/local/bin'],
         before   => Exec['reload'],
