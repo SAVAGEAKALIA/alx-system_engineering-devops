afc4a5f092235b319fbc4760268d991b83fdd810
SAVAGEAKALIA <akaliasaviour@gmail.com>
2024-09-03 06:03:56 +0100

    webstack debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Ensure the WordPress configuration is fixed
 exec { 'update-wp-settings':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => ['/bin', '/usr/bin', '/usr/sbin'],
   onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
   unless  => 'grep -q php /var/www/html/wp-settings.php',
